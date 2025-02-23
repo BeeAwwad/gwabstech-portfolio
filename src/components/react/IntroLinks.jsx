e71363bcb82c097ea1a9167987197a65fd61dd2d
BeeAwwad <aosalihu@gmail.com>
2025-02-23 09:37:55 +0100

    Add fonts

--- src/components/react/IntroLinks.jsx
+++ src/components/react/IntroLinks.jsx
@@ -4,7 +4,7 @@ import React from "react"
 const IntroLinks = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-7 max-w-sm md:max-w-2xl lg:max-w-5xl mx-auto gap-4">
-      <div className="bg-[#31363F] rounded-xl flex flex-col justify-between p-5 xl:col-span-3 space-y-5 text-[#eee]">
+      <div className="bg-red-900 rounded-xl flex flex-col justify-between p-5 xl:col-span-3 space-y-5 text-[#eee]">
         <Icon className="text-5xl" icon={"ph:briefcase"} />
         <div className="flex flex-col gap-5">
           <p className="text-2xl max-w-96">
@@ -12,7 +12,7 @@ const IntroLinks = () => {
           </p>
           <a
             href="/experience"
-            className="p-1.5 hover:bg-[#eee] hover:text-[#31363F] transition-colors border-2 border-white w-fit ml-auto"
+            className="p-1.5 hover:bg-[#eee] hover:text-red-900 transition-colors border-2 border-[#eee] w-fit ml-auto"
           >
             <Icon className="text-2xl" icon={"basil:arrow-right-solid"} />
           </a>
